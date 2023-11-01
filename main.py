8f4b32fc02cea00abd64b52bb9c80ed74d5d2d06
shawn <shianhian327@gmail.com>
2023-11-01 14:56:38 +0800

    Updated to version 0.3

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from trendychat.module2 import test2_print
 
 test1_print()
 test2_print()
-
+print("it's good.")
 # pip install git+https://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@github.com/shianshiu/trendychat.git
